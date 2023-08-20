ef640a70e35f7ef739bc43cab945b328d6905f42
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-08-20 11:47:53 +0700

    updated text

--- src/app/components/sections/SharePhotosSection.tsx
+++ src/app/components/sections/SharePhotosSection.tsx
@@ -6,7 +6,7 @@ interface Props {}
 const SharePhotosSection = ({}: Props) => {
   return (
     <SectionWrapper className="bg-white w-screen py-32 md:py-40 font-[SukhumvitSet]">
-      <h2 className="heading2 text-center">Photos</h2>
+      <h2 className="heading2 text-center">Photos At The Event</h2>
 
       <div className="mt-8 text-center flex flex-col gap-4 md:gap-8 text- md:text-2xl hover:cursor-pointer underline">
         <a href="https://fotoshare.co/u/289040777/3630274" target="_blank">
