2a3c40ed11703f4c531c04dcc574c2a95df01a8c
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-08-20 11:47:21 +0700

    add link to photo booth

--- src/app/components/sections/SharePhotosSection.tsx
+++ src/app/components/sections/SharePhotosSection.tsx
@@ -8,7 +8,10 @@ const SharePhotosSection = ({}: Props) => {
     <SectionWrapper className="bg-white w-screen py-32 md:py-40 font-[SukhumvitSet]">
       <h2 className="heading2 text-center">Photos</h2>
 
-      <div className="mt-8 text-center text-lg md:text-2xl hover:cursor-pointer underline">
+      <div className="mt-8 text-center flex flex-col gap-4 md:gap-8 text- md:text-2xl hover:cursor-pointer underline">
+        <a href="https://fotoshare.co/u/289040777/3630274" target="_blank">
+          😎 From Photo Booth
+        </a>
         <a
           href="https://www.flickr.com/gp/168220958@N08/C65hr714Le"
           target="_blank"
