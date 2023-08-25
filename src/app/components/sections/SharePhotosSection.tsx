46a169859c918395870313359b322dd117a880ff
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-08-25 18:35:08 +0700

    added video

--- src/app/components/sections/SharePhotosSection.tsx
+++ src/app/components/sections/SharePhotosSection.tsx
@@ -1,30 +1,79 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import SectionWrapper from "../wrappers/SectionWrapper";
+import AnimateBlob from "../AnimateBlob";
+import FireshipBlob34 from "../svg/FireshipBlob34";
 
 interface Props {}
 
 const SharePhotosSection = ({}: Props) => {
   return (
-    <SectionWrapper className="bg-white w-screen py-32 md:py-40 font-[SukhumvitSet]">
-      <h2 className="heading2 text-center">Photos On The Wedding Day</h2>
+    <SectionWrapper className="bg-white w-screen py-32 md:py-40 font-[SukhumvitSet] max-w-7xl">
+      <div className="flex justify-between items-center flex-col lg:flex-row mx-10">
+        <div>
+          <h2 className="heading2 text-center">Photos On The Wedding Day</h2>
 
-      <div className="mt-8 text-center flex flex-col gap-4 md:gap-8 text- md:text-2xl hover:cursor-pointer underline">
-        <a
-          href="https://drive.google.com/drive/folders/1hNQ-i5xZt0JaPjwJlcYw30hIH-AHGM96"
-          target="_blank"
-        >
-          💍 From Engagement Ceremony
-        </a>
-        <a href="https://fotoshare.co/u/289040777/3630274" target="_blank">
-          😎 From Photo Booth
-        </a>
-        <a
-          href="https://www.flickr.com/gp/168220958@N08/C65hr714Le"
-          target="_blank"
-        >
-          📷 From Shane&#39;s Camera
-        </a>
+          <div className="mt-8 text-center flex flex-col gap-4 md:gap-8 text- md:text-2xl hover:cursor-pointer underline">
+            <a
+              href="https://photos.app.goo.gl/93QQX2uY2DAre8if6"
+              target="_blank"
+            >
+              💍 From Engagement Ceremony
+            </a>
+            <a href="https://fotoshare.co/u/289040777/3630274" target="_blank">
+              😎 From Photo Booth
+            </a>
+            <a
+              href="https://www.flickr.com/gp/168220958@N08/C65hr714Le"
+              target="_blank"
+            >
+              📷 From Shane&#39;s Camera
+            </a>
+          </div>
+        </div>
+
+        <div>
+          {/* mobile */}
+
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/tGIqzuEtbdA?si=iLwoYR2NWJgVrOSy"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            className="mt-20 lg:hidden max-w-[280px] max-h-[150px]"
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/tGIqzuEtbdA?si=iLwoYR2NWJgVrOSy"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            className="hidden lg:block"
+          ></iframe>
+        </div>
       </div>
+
+      <AnimateBlob
+        blobId1="#blob3"
+        blobId2="#blob4"
+        image={
+          <div className="translate-x-[calc(8%-24px)] translate-y-1/3 absolute z-10 top-0 left-0 ">
+            <img
+              alt="flower"
+              src="/images/flower-2.svg"
+              className="h-[100px] md:h-[150px] animate-wiggle"
+            />
+          </div>
+        }
+        blob={
+          <FireshipBlob34 className="w-60 h-60 md:w-80 md:h-80 absolute top-0 left-0 z-0  -translate-x-[calc(35%)] -translate-y-1/4" />
+        }
+      />
     </SectionWrapper>
   );
 };
