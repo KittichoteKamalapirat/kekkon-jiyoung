065f374d90ecd5830a8c01ab24b61ee0eaafeae0
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-08-25 18:12:36 +0700

    added photo link to engagement ceremony

--- src/app/components/sections/SharePhotosSection.tsx
+++ src/app/components/sections/SharePhotosSection.tsx
@@ -9,6 +9,12 @@ const SharePhotosSection = ({}: Props) => {
       <h2 className="heading2 text-center">Photos On The Wedding Day</h2>
 
       <div className="mt-8 text-center flex flex-col gap-4 md:gap-8 text- md:text-2xl hover:cursor-pointer underline">
+        <a
+          href="https://drive.google.com/drive/folders/1hNQ-i5xZt0JaPjwJlcYw30hIH-AHGM96"
+          target="_blank"
+        >
+          💍 From Engagement Ceremony
+        </a>
         <a href="https://fotoshare.co/u/289040777/3630274" target="_blank">
           😎 From Photo Booth
         </a>
