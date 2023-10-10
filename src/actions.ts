b91d58421b675d1577b7b4fd10692974d23d76d9
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2023-10-10 12:50:28 +0700

    can read and write form google sheets with googleapis 2

--- src/actions.ts
+++ src/actions.ts
@@ -64,3 +64,39 @@ export async function postToGoogleSheets(data: RsvpFormValues) {
     console.error("error in server action", error);
   }
 }
+export async function getFromGoogleSheets(data: RsvpFormValues) {
+  try {
+    // Auth
+    const auth = await google.auth.getClient({
+      scopes: [
+        "https://www.googleapis.com/auth/spreadsheets", // for write
+      ],
+    });
+
+    const sheets = google.sheets({ version: "v4", auth });
+
+    // Query
+
+    const range = `dev`;
+
+    const { firstName, lastName } = data;
+    const rowData = [[firstName, lastName, "phone"]]; // replace with your actual row data
+
+    const request = {
+      spreadsheetId: process.env.GOOGLE_SHEET_ID, // replace with your Spreadsheet ID
+      range: range,
+      valueInputOption: "USER_ENTERED", // or "RAW", see link below
+      insertDataOption: "INSERT_ROWS", // see link below
+      resource: {
+        values: rowData,
+      },
+      auth: auth,
+    };
+
+    const response = await sheets.spreadsheets.values.append(request);
+    // console.log("result", result.data);
+    return response;
+  } catch (error) {
+    console.error("error in server action", error);
+  }
+}
