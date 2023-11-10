61e82da43d455fd88d06ef04039a309f72484f26
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2023-11-10 22:15:18 +0700

    server action: added log for env vars

--- src/actions.ts
+++ src/actions.ts
@@ -31,6 +31,16 @@ export async function postToGoogleSheets(
       },
     });
 
+    console.log("process.env.GOOGLE_PROJECT_ID", process.env.GOOGLE_PROJECT_ID);
+    console.log(
+      "process.env.GOOGLE_PRIVATE_KEY",
+      process.env.GOOGLE_PRIVATE_KEY
+    );
+    console.log(
+      "process.env.GOOGLE_CLIENT_EMAIL",
+      process.env.GOOGLE_CLIENT_EMAIL
+    );
+
     const sheets = google.sheets({ version: "v4", auth });
 
     const range = `dev`;
