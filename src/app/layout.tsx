178d317ccdc494f01b0806ded91654d5815b8a85
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2023-11-13 21:51:06 +0700

    add twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Jiyoung & Rodrigo",
   description: "Jiyoung & Rodrigo Wedding Invitation",
+
   openGraph: {
+    title: "Jiyoung & Rodrigo",
+    description: "Jiyoung & Rodrigo Wedding Invitation",
+    url: "https://kekkon-jiyoung.vercel.app",
+    type: "website",
+    images: "/images/logo/android-chrome-512x512.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jiyoung & Rodrigo",
+    description: "Jiyoung & Rodrigo Wedding Invitation",
+    creator: "@Shaneinsane6",
     images: "/images/logo/android-chrome-512x512.png",
   },
 };
